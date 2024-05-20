2f0698354274849fd62f46a4a6d5a7f0700bba5e
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-05-20 12:52:54 +0000

    Adding schema for the restPassword

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,7 +9,6 @@ export const LoginSchema = z.object({
     password: z.string().min(6, {
     message: "Password must be at least 6 characters",
     }),
-    code:z.optional(z.string())
 });
     
 
@@ -23,4 +22,16 @@ export const RegisterSchema = z.object({
     name: z.string().min(1, {
         message: "Name is required"
     })
+});
+
+export const ResetPasswordSchema = z.object({
+    email: z.string().email({
+    message: "Please enter a valid email",
+    }),
+});
+
+export const NewPasswordSchema = z.object({
+    password: z.string().min(6, {
+    message: "Password must be at least 6 characters",
+    }),
 });
