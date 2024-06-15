cd41bac6bfe3e23ac34b91c959c669aa93dfa0da
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-15 15:03:50 +0000

    Adding both signIn and up Page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,11 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 const AuthLogin = () => {
   return (
-    <>
+    <Suspense>
       <LoginForm />
-    </>
+    </Suspense>
   );
 };
 
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,10 +1,11 @@
 import { RegisterForm } from "@/components/auth/register-form";
+import { Suspense } from "react";
 
 const RegisterPage = () => {
     return ( 
-        <>
+        <Suspense>
             <RegisterForm />
-        </>
+        </Suspense>
      );
 }
  
