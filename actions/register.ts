f6738ae5e87c51bfdd46dc39a2bcec2928d283b5
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-15 15:40:38 +0000

    Server action for register an account

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 import bcrypt from "bcryptjs"
 
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFileds = RegisterSchema.safeParse(values)
@@ -14,5 +16,20 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
     const { name, email, password } = validatedFileds.data;
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+        return {error: "Email already in use !"}
+    }
+
+    await db.user.create({
+        data: {
+            name, 
+            email,
+            password: hashedPassword,
+        }
+    })
+
+   
     
 }
