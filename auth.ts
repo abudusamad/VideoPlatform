ee6732ef00d57a4e889d63fea352a756f32ebb75
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-15 21:16:06 +0000

    Extending the sesstion for jwt

--- auth.ts
+++ auth.ts
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+
+import { db } from "@/lib/db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt"
+  },
   providers: [],
 })
