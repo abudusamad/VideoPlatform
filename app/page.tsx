2e3b0de697f2e3a49012aee8de880e802a1f240f
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-20 11:33:10 +0000

    Index page: [12]

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold text-emerald-500">
-      Hellow  world
-   </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔒 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+      </div>
+      <div>
+        <Button variant="secondary" size="lg">
+          Sign In
+        </Button>
+      </div>
+    </main>
   );
 }
