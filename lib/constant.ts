9286de0cca507b5c4cf71f27a03cc5d53e68bb12
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-20 01:28:30 +0000

    Making the conference [#1]

---
+++ lib/constant.ts
@@ -0,0 +1,15 @@
+export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
+export const TWITTER_USER_NAME = 'vercel';
+export const BRAND_NAME = 'ACME';
+export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
+export const SITE_NAME = 'ACME Conf';
+export const META_DESCRIPTION =
+  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+export const SITE_DESCRIPTION =
+  'An interactive online experience by the community, free for everyone.';
+export const DATE = '01 January 2022';
+export const SHORT_DATE = 'Jan 1 - 9:00am PST';
+export const FULL_DATE = 'Jan 1st 9am Pacific Time (GMT-7)';
+export const TWEET_TEXT = META_DESCRIPTION;
+export const COOKIE = 'user-id';
