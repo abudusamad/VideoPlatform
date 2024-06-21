a645509d5a062697ec04c3076d8ebd4857d585f0
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-21 11:27:43 +0000

    Finished with eamil verication

---
+++ app/auth/confirm/page.tsx
@@ -0,0 +1,13 @@
+
+import { ConfirmForm } from "@/components/confirm-form";
+import { Suspense } from "react";
+
+const ConfirmPage = () => {
+  return (
+    <Suspense>
+      <ConfirmForm/>
+    </Suspense>
+  );
+};
+
+export default ConfirmPage;
--- routes.ts
+++ routes.ts
@@ -5,7 +5,7 @@
  */
 export const publicRoutes = [
   "/",
-  "/auth/new-verification"
+  "/auth/confirm",
 ];
 
 /**
