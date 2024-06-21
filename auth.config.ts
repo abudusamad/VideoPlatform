461ce29604ae0ce150c409e14d8198f4c7ebb52d
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-21 00:47:08 +0000

    Adding the gihub and google account

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,8 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 
 
 import { LoginSchema } from "./schemas";
@@ -8,6 +10,17 @@ import { getUserByEmail } from "./data/user";
 
 export default {
     providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        
+        }), 
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+
+        }),
+
         Credentials({
             async authorize(credentials) {
                 const validatedFields = LoginSchema.safeParse(credentials);
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,16 +1,38 @@
+"use client";
+
 import { FcGoogle } from "react-icons/fc";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { FaGithub } from "react-icons/fa";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useSearchParams } from "next/navigation";
 
 export const Social = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button variant="outline" size="lg"  className="w-full"  >
+      <Button
+        variant="outline"
+        size="lg"
+        className="w-full"
+        onClick={() => onClick("google")}
+      >
         <FcGoogle className="h-5 w-5" />
-          </Button>
-          <Button variant="outline" size="lg" className="w-full"  >
-              <FaGithub className="h-5 w-5" />
-            </Button>
+      </Button>
+      <Button
+        variant="outline"
+        size="lg"
+        className="w-full"
+        onClick={() => onClick("github")}
+      >
+        <FaGithub className="h-5 w-5" />
+      </Button>
     </div>
   );
 };
