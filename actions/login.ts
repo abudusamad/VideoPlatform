2536a6f914daab979d505732a34e3e28d3b4608c
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-21 11:08:13 +0000

    Adding the verication token to the action login

--- actions/login.ts
+++ actions/login.ts
@@ -8,23 +8,37 @@ import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 
 export const login = async (values: z.infer<typeof LoginSchema>, callbackUrl?:string | null) => {
-	const validatedFields = LoginSchema.safeParse(values);
-	
-	console.log("Login:", values);
+ const validatedFields = LoginSchema.safeParse(values);
 
-    if(!validatedFields.success) {
-        return {error: "Invalid fields!"}
-    }
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
 
-    const { email, password} = validatedFields.data;
-    const exitingUser = await getUserByEmail(email);
+  const { email, password, code } = validatedFields.data;
 
-    if (!exitingUser || !exitingUser.email || !exitingUser.password ) {
-        return {error: "Email does not exit!"}
-    }
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email does not exist!" }
+  }
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email,
+    );
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token,
+    );
+
+    return { success: "Confirmation email sent!" };
+  }
     
 	try {
 		await signIn("credentials", {
@@ -33,7 +47,7 @@ export const login = async (values: z.infer<typeof LoginSchema>, callbackUrl?:st
 			redirectTo: DEFAULT_LOGIN_REDIRECT,
 		
 		});
-		return { success: "Logged in successfully!" };
+		
 	} catch (error) {
 		if (error instanceof AuthError) {
 			switch (error.type) {
