8a1bb71ac229735ac59a4d08f4dbb01efaba504b
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-21 14:48:41 +0000

    Adding forms to add newpassword when reset

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,14 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form";
+import { Suspense } from "react";
+
+
+const NewPasswordPage = () => {
+
+    return (
+        <Suspense>
+            <NewPasswordForm />
+    </Suspense>
+)
+};
+
+export default NewPasswordPage;
