e75435256f0cf116a09fa3639ded9ac50e79e9c6
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-21 00:03:52 +0000

    Adding middleware to control the page

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,53 @@
-export { auth as middleware } from "@/auth"
+import NextAuth from "next-auth";
+
+import authConfig from "@/auth.config";
+import {
+	DEFAULT_LOGIN_REDIRECT,
+	apiAuthPrefix,
+	authRoutes,
+	publicRoutes,
+} from "@/routes";
+import { NextRequest } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+	
+	const { nextUrl } = req;
+	const isLoggedIn = !!req.auth;
+
+	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+	const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+	if (isApiAuthRoute) {
+		return;
+	}
+
+	if (isAuthRoute) {
+		if (isLoggedIn) {
+			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+		}
+		return ;
+	}
+
+	if (!isLoggedIn && !isPublicRoute) {
+		let callbackUrl = nextUrl.pathname;
+		if (nextUrl.search) {
+			callbackUrl += nextUrl.search;
+		}
+
+		const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+		return Response.redirect(
+			new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+		);
+	}
+
+	return;
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
