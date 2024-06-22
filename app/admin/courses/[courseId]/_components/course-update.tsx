73ffc5a32e0f89c690aa04dbcfc3979111fdbc50
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-22 23:26:07 +0000

    creating Course update

---
+++ app/admin/courses/[courseId]/_components/course-update.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "react-toastify";
+import { CreateVideoSchema } from "@/schemas";
+
+interface CourseUpdateProps {
+  initialData: {
+      name: string;
+      description: string;
+  };
+  courseId: string;
+}
+
+
+
+export const CourseUpdate = ({ initialData, courseId }: CourseUpdateProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof CreateVideoSchema>>({
+    resolver: zodResolver(CreateVideoSchema),
+    defaultValues: initialData,
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof CreateVideoSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course updated");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+      Course Update
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit Course
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <div className="flex flex-col gap-2">
+          <p className="text-sm mt-2">{initialData.name}</p>
+          <p className="text-sm mt-2">{initialData.description}</p>
+        </div>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Advanced web development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Advanced web development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button disabled={!isValid || isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
--- app/admin/courses/[courseId]/page.tsx
+++ app/admin/courses/[courseId]/page.tsx
@@ -1,6 +1,8 @@
 import getCurrentUser from "@/actions/get-current-user";
+import { Container } from "@/components/container";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { CourseUpdate } from "./_components/course-update";
 
 const CourseIdPage = async ({
   params,
@@ -14,12 +16,43 @@ const CourseIdPage = async ({
     return redirect("/auth/login");
   }
 
-    const course = await db.course.findUnique({
+  const course = await db.course.findUnique({
     where: {
-            id: params.courseId,
-        authorId: currentUser.id,
+      id: params.courseId,
+      authorId: currentUser.id,
     },
-    })
-  return <div>this is course Id page</div>;
+  });
+  if (!course) {
+    return redirect("/admin/courses");
+  }
+  const requiredFields = [course.name, course.description];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
+  const isComplete = requiredFields.every(Boolean);
+
+  return (
+    <Container>
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-3xl font-medium">Course setup</h1>
+            <span className="text-lg text-slate-700">
+              Complete all fields {completionText}
+            </span>
+          </div>
+          {/* <ActionForm
+            disabled={!isComplete}
+            courseId={params.courseId}
+            isPublished={course.isPublished}
+          /> */}
+        </div>
+        <div>
+          <CourseUpdate initialData={course} courseId={course.id} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 export default CourseIdPage;
