19e103632b86959886763799434a82b3ba2e5f23
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-22 03:26:13 +0000

    Adding placeholder image

--- app/(dashboard)/_components/avatarImage.tsx
+++ app/(dashboard)/_components/avatarImage.tsx
@@ -11,10 +11,7 @@ export const AvatarImg = ({ src, alt, onClick, className }: AvatarImgProps) => {
   return (
     <Avatar onClick={onClick}>
       <AvatarImage
-        src={
-          src ||
-          "https://miro.medium.com/v2/resize:fill:64:64/1*dmbNkD5D-u45r44go_cf0g.png"
-        }
+        src={src || "/placeholder.jpg"}
         alt={alt}
         className={className}
       />
