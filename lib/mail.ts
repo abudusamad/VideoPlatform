76c693bb0665529bf4384d01506fc0306167d2d4
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-28 08:18:35 +0000

    Finished with password reset

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,6 +4,24 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_URL;
 
+export const sendPasswordResetEmail = async (
+  token: string,
+  email: string,
+) => {
+  const resetLink = `${domain}/auth/new-password?token=${token}`
+
+  await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: email,
+    subject: "Reset your password",
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
+
+  });
+  console.log(
+    token, email, resetLink,
+  )
+};
+
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/confirm?token=${token}`;
 
@@ -13,18 +31,9 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
   });
+
+   console.log(
+    token, email, confirmLink,
+  )
 };
 
-export const sendPasswordResetEmail = async (
-  email: string,
-  token: string,
-) => {
-  const resetLink = `${domain}/auth/new-password?token=${token}`
-
-  await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-    to: email,
-    subject: "Reset your password",
-    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
-  });
-};
