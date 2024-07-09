66b0b856cb555c74227191439d0f2489428aa62a
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-09 21:20:28 +0000

    Making the filtering query

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -8,26 +8,33 @@ import { UserRole } from "@prisma/client";
 import { FormSuccessDb } from "@/components/form-sucess-db";
 import { currentRole } from "@/lib/auth";
 import { getCourses } from "@/actions/get-courses";
+import { Suspense } from "react";
 
+interface searchParams {
+  name: string;
+}
 
-const HomePage = async () => {
-  const course = await getCourses({});
+const HomePage = async ({ searchParams }: { searchParams: searchParams }) => {
+  const course = await getCourses(searchParams);
+
   const currentUserRole = await currentRole();
 
   return (
     <main className="mt-20">
       <Container>
-        <div className="flex flex-col gap-4 ">
-          <RoleGate allowedRole={UserRole.ADMIN}>
-            <FormSuccessDb message="You have permission  Add Course!" />
-          </RoleGate>
-          {currentUserRole === UserRole.ADMIN && (
-            <Link href="/admin/courses">
-              <Button variant="blue">Go to Admin</Button>
-            </Link>
-          )}
-        </div>
-        <CoursesList items={course} />
+        <Suspense>
+          <div className="flex flex-col gap-4 ">
+            <RoleGate allowedRole={UserRole.ADMIN}>
+              <FormSuccessDb message="You have permission  Add Course!" />
+            </RoleGate>
+            {currentUserRole === UserRole.ADMIN && (
+              <Link href="/admin/courses">
+                <Button variant="blue">Go to Admin</Button>
+              </Link>
+            )}
+          </div>
+          <CoursesList items={course} />
+        </Suspense>
       </Container>
     </main>
   );
